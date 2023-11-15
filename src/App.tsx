d527f3c6df016965ab88e9bec438f8ef0ec2d1fa
JOmega12 <omega90ej@gmail.com>
2023-11-14 23:02:08 -0800

    new skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 // import './App.css'
 
 import { Route, Routes } from "react-router-dom";
@@ -8,22 +9,25 @@ import { AuthProvider } from "./Providers/AuthProvider";
 import { Lobby } from "./Pages/Lobby";
 import { DogCard } from "./Pages/DogCard";
 
+
 function App() {
+
+
   return (
     <>
-      <AuthProvider>
-        <Navbar />
-        <div className="py-10 mx-10 ">
-          <Routes>
-            <Route path="/" element={<Homepage />}></Route>
-            <Route path="about-us" element={<AboutPage />}></Route>
-            <Route path="lobby" element={<Lobby />}></Route>
-            <Route path="dogs" element={<DogCard />}></Route>
-          </Routes>
-        </div>
-      </AuthProvider>
+    <AuthProvider>
+      <Navbar/>
+      <div className="py-10 mx-10 ">
+        <Routes>
+          <Route path="/" element={<Homepage/>}></Route>
+          <Route path="about-us" element={<AboutPage/>}></Route>
+          <Route path="lobby" element={<Lobby/>}></Route>
+          <Route path="dogs" element={<DogCard />}></Route>
+        </Routes>
+      </div>
+    </AuthProvider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
