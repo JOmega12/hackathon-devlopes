0b57c7bfca9ef4c6b3377174307d8d42b10ed6c0
JOmega12 <omega90ej@gmail.com>
2023-11-15 20:44:41 -0800

    added apis context and testing homepage of dogs

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,9 +1,39 @@
+import { useDog } from "../Providers/DogProvider"
 
 
 export const Homepage = () => {
+    const {dogs} = useDog();
 
+    console.log(dogs, 'dog list')
 
     return(
+        <>
         <div>this is homepage component</div>
-    )
+        
+        {/* !THIS IS A TEST */}
+
+        <div>
+            <div>
+                <h1>dog test</h1>
+
+                { dogs && Array.isArray(dogs) ? (
+                    dogs.map((dog, index) => (
+                            <div key={index}>
+                                <h1>{dog.name}</h1>
+                                <h3>{dog.breed}</h3>
+                                <div>
+                                    <p>Is it available?</p>
+                                    <p>{dog.available}</p>
+                                </div>
+                            </div>
+                    )) 
+                    ) : (
+                    <div>theres none</div>
+                )}
+            </div>
+
+        </div>
+        </>
+
+    )  
 }
--- src/Providers/BookingProvider.tsx
+++ src/Providers/BookingProvider.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, ReactNode, SetStateAction, createContext, useContext, useEffect, useState } from "react";
 import { DogTypes } from "../types"
 import { getAllDogs } from "../api/getAllDogs";
+import { toggleBookingAPI } from "../api/BookingsAPI";
 
 
 
@@ -21,9 +22,8 @@ const BookingContext = createContext<TBookingContext | undefined>(undefined);
 export const BookingProvider = ({children}: BookContextProps) => {
     const [bookDog, setBookDog]= useState<DogTypes | null>(null)
     
-    // need to build api
     const refetch = () => {
-        getAllDogs.then(setBookDog);
+        getAllDogs().then(setBookDog);
     }
 
     useEffect(() => {
@@ -44,6 +44,11 @@ export const BookingProvider = ({children}: BookContextProps) => {
 }
 
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useBooking = () => {
-    const context = useContext(BookingContext)
+    const context = useContext(BookingContext);
+    if(!context) {
+        throw new Error('Please use the useFavorite Context');
+    }
+    return context;
 }
