356133a44d6ae1dd1c8ae2ca30a988705b94b3b4
JOmega12 <omega90ej@gmail.com>
2023-11-17 22:55:12 -0800

    fixed the bug

--- src/Pages/BookedDogs.tsx
+++ src/Pages/BookedDogs.tsx
@@ -17,14 +17,14 @@ export const BookedDogs = () => {
   );
 
   // this should show the data that the dog is unavailable
-
+    // console.log(bookDogsData, 'booksDogData')
 
   useEffect(() => {
     if (Array.isArray(bookDog)) {
       // when user is loggedIn, this shows what the user has booked
       const userBookings = bookDog.filter(
         (selfUser: { userId: number | undefined }) => {
-            console.log(user)
+            // console.log(user)
           return selfUser.userId === user?.id;
         }
       );
@@ -35,31 +35,27 @@ export const BookedDogs = () => {
               return specificDogId.id === bookDogId.dogId;
             });
           });
+          // console.log(matchedData, 'matchedData')
           setBookDogsData(matchedData);
       }
     
     }
   }, [bookDog, dogs, user, user?.id]);
 
-  console.log(bookDogsData, "bookdogsData");
+  // console.log(bookDogsData, "bookdogsData");
 
   // onClick this either creates a booking or does not book the person
   const onBookingClick = (dogId: number | undefined) => {
-    if (user && user?.id && Array.isArray(bookDog) &&dogId !== undefined) {
+    if (user && user?.id && Array.isArray(bookDog) && dogId !== undefined) {
       // Check if the dog is currently booked
       const isBooked = bookDog.some((booking) => booking.dogId === dogId && booking.userId === user?.id);
-  
+      console.log(isBooked, 'isbooked')
       if (isBooked) {
-        // Unbook the dog
         toggleBooking({
           dogId: dogId,
           userId: user?.id,
         });
   
-        // Remove the unbooked dog from the state
-        setBookDogsData((prevBookDogsData) =>
-          prevBookDogsData.filter((dog) => dog?.id !== dogId)
-        );
       }
     }
   };
@@ -68,7 +64,7 @@ export const BookedDogs = () => {
     <div>
       <div className="flex flex-col min-h-screen flex-grow mx-auto p-8 ">
         <h1 className="text-6xl font-extrabold text-center mb-16">Dogs You Booked</h1>
-        <div className="flex justify-center">
+        <div className="flex justify-center mb-10">
           <button
             onClick={() => navigate("/lobby")}
             className=" bg-pink-btn p-5 text-2xl text-off-white"
@@ -80,24 +76,30 @@ export const BookedDogs = () => {
           {bookDogsData && Array.isArray(bookDogsData) && user && isRegister ? (
             bookDogsData.map((dog, index) => (
               <div key={index} className="flex flex-col">
-                <div className="">
-                  <img
-                    src={dog?.image}
-                    alt="dog image"
-                    className="w-[250px] h-[250px]"
-                  />
-                </div>
-                <div className="flex flex-col text-center gap-2">
-                  <h2 className="text-4xl font-bold">{dog?.name}</h2>
-                  <h3 className="text-2xl font-semibold">{dog?.breed}</h3>
-                  <div>
-                    <p>Is it available?</p>
-                    <p>{dog?.timeAvailable}</p>
-                  </div>
-                  <button onClick={() => onBookingClick(dog?.id)}>
-                    Book now!
-                  </button>
-                </div>
+                {dog && (
+                  <>
+                    <div className="">
+                      <img
+                        src={dog.image}
+                        alt="dog image"
+                        className="w-[250px] h-[250px]"
+                      />
+                    </div>
+                    <div className="flex flex-col text-center gap-2">
+                      <h2 className="text-4xl font-bold">{dog.name}</h2>
+                      <h3 className="text-2xl font-semibold">{dog.breed}</h3>
+                      <div>
+                        <p>Is it available?</p>
+                        <p>{dog.timeAvailable}</p>
+                      </div>
+                      <button onClick={() => onBookingClick(dog.id)}
+                        className={`border-4 ${dog.available ? '' : 'bg-orange-bg'}`}
+                      >
+                        Book now!
+                      </button>
+                    </div>
+                  </>
+                )}
               </div>
             ))
           ) : (
@@ -110,7 +112,6 @@ export const BookedDogs = () => {
 
       <div></div>
     </div>
-    //     const updatedActiveDogs = allActiveDogs?.filter((dog) => dog.id !== dogId)
   );
 };
 
